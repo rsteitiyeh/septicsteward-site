9e5aca730f875d131448573c779d73a375bd6a50
rsteitiyeh <rami@trilot.com>
2026-07-12 21:48:45 +0300

    update build.js

--- build.js
+++ build.js
@@ -209,6 +209,6 @@ const urls = [`${SITE}/`, `${SITE}/guides/`]
 fs.writeFileSync(path.join(DIST, "sitemap.xml"),
   `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
   urls.map((u) => `  <url><loc>${u}</loc></url>`).join("\n") + `\n</urlset>\n`);
-fs.writeFileSync(path.join(DIST, "robots.txt"), `User-agent: *\nDisallow: /\n`);
+fs.writeFileSync(path.join(DIST, "robots.txt"), `User-agent: *\nAllow: /\n`);
 
 console.log(`[build] ${articles.length} articles, ${urls.length} urls -> dist/`);
